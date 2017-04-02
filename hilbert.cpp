74b0d9cc1255fbc8734be665eb8175a31cc2c23d
Kiran Bhadury <kbhadury@ucdavis.edu>
2017-04-01 18:31:11 -0700

    Added support for leaves, thinner cylinders

--- hilbert.cpp
+++ hilbert.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 #include <cmath>
 
+/***** CONTENTS *****\
+
+
+   Public methods
+   Color and drawing
+   Parsing
+   OpenGL functions
+
+
+\*******************/
+
+
 //Part of the member function workaround (see the .h file for details)
 Hilbert* Hilbert::curObj = 0;
 
@@ -13,12 +25,12 @@ bool up;
 int deg = 0;
 
 
-/**** Public methods *****/
+/***** Public methods *****/
 
 //Constructor, describes L-system
 Hilbert::Hilbert(string start, string vars, string* rules, float ang, float side)
 {
-  consts = "F+-&^\\/|[]`~"; //Characters that we recognize as constants
+  consts = "F+-&^\\/|[]`~$"; //Characters that we recognize as constants
   debug = false;
 
   startStr = start;
@@ -28,7 +40,7 @@ Hilbert::Hilbert(string start, string vars, string* rules, float ang, float side
   sideLen = side;
 
   level = 0;
-  userShape = CUBE;
+  userShape = CUBE; //Default shape
 }
 
 //Enable drawing a coordinate system and printing the rotation angle
@@ -37,6 +49,7 @@ void Hilbert::enableDebug(bool enable)
   debug = enable;
 }
 
+//Allow user to specify the shape used in constructing the L-system
 void Hilbert::setShape(int shape)
 {
   if(shape < 0 || shape > 2)
@@ -59,9 +72,10 @@ void Hilbert::draw(int argc, char** argv, int lvl)
 }
 
 
-/**** Color and drawing *****/
+/***** Color and drawing *****/
 
 //Draw a pyramid of the given height (height is equal to base length)
+//When centered at the origin, it points along the positive y-axis
 void Hilbert::drawPyra(float height)
 {
   //Vertices
@@ -110,65 +124,65 @@ void Hilbert::drawPyra(float height)
   glEnd();
 }
 
+//Draw a cylinder with diameter and height equal to len
+//Sides are constructed with 10 faces
+//Centered at the origin with the z-axis passing through the circular faces
 void Hilbert::drawCyl(float len)
 {
-  GLfloat bottomFace[3*15];
-  GLfloat topFace[3*15];
+  GLfloat bottomFace[3*10];
+  GLfloat topFace[3*10];
   
   //This can probably be made more efficient...
 
+  float convert = 3.14159/180; //Convert from degrees to radians
+
   //Initialize vertices
-  float convert = 3.14159/180;
-  for(int i = 0; i < 15; ++i)
+  for(int i = 0; i < 10; ++i)
   {
-    float angle = 24*i*convert;
-    bottomFace[3*i]   = topFace[3*i] = len*cos(angle);
-    bottomFace[3*i+1] = -len/2;
-    topFace[3*i+1]    = len/2;
-    bottomFace[3*i+2] = topFace[3*i+2] = len*sin(angle);
+    float angle = 36*i*convert;
+    bottomFace[3*i] = topFace[3*i] = (len/2)*sin(angle);
+    bottomFace[3*i+1] = topFace[3*i+1] = (len/2)*cos(angle);
+    bottomFace[3*i+2] = -len/2;
+    topFace[3*i+2] = len/2;
   }
 
   //Construct bottom face
   glBegin(GL_POLYGON);
     glNormal3f(0, 0, -1);
-    for(int i = 0; i < 15; ++i)
+    for(int i = 0; i < 10; ++i) //CCW winding
     {
-      glVertex3f(bottomFace[3*i+2], bottomFace[3*i], bottomFace[3*i+1]);
+      glVertex3f(bottomFace[3*i], bottomFace[3*i+1], bottomFace[3*i+2]);
     }
   glEnd();
 
   //Construct top face
   glBegin(GL_POLYGON);
     glNormal3f(0, 0, 1);
-    for(int i = 14; i >= 0; --i)
+    for(int i = 9; i >= 0; --i) //CCW winding
     {
-      glVertex3f(topFace[3*i+2], topFace[3*i], topFace[3*i+1]);
+      glVertex3f(topFace[3*i], topFace[3*i+1], topFace[3*i+2]);
     }
   glEnd();
 
   //Construct sides
-  int offset = (15/2) + 1;
-  int numsides = 15;
-  float dtheta = 24*convert;
-  int multiplier;
+  float offset = 18*convert;
+  float dtheta = 36*convert;
   glBegin(GL_QUADS);
-    for(int i = 0; i <= 13; ++i)
+    for(int i = 0; i <= 8; ++i)
     {
-      multiplier = (i+offset) % numsides;
-      glNormal3f(-1*sin(multiplier*dtheta), -1*cos(multiplier*dtheta), 0);
-      glVertex3f(topFace[3*i+2], topFace[3*i], topFace[3*i+1]);
-      glVertex3f(topFace[3*i+5], topFace[3*i+3], topFace[3*i+4]);
-      glVertex3f(bottomFace[3*i+5], bottomFace[3*i+3], bottomFace[3*i+4]);
-      glVertex3f(bottomFace[3*i+2], bottomFace[3*i], bottomFace[3*i+1]);
+      glNormal3f(sin(offset + i*dtheta), cos(offset + i*dtheta), 0);
+      glVertex3f(topFace[3*i], topFace[3*i+1], topFace[3*i+2]);
+      glVertex3f(topFace[3*i+3], topFace[3*i+4], topFace[3*i+5]);
+      glVertex3f(bottomFace[3*i+3], bottomFace[3*i+4], bottomFace[3*i+5]);
+      glVertex3f(bottomFace[3*i], bottomFace[3*i+1], bottomFace[3*i+2]);
     }
     
     //Add the last rectangle
-    multiplier = (14+offset) % numsides;
-    glNormal3f(-1*sin(multiplier*dtheta), -1*cos(multiplier*dtheta), 0);
-    glVertex3f(topFace[3*14+2], topFace[3*14], topFace[3*14+1]);
-    glVertex3f(topFace[2], topFace[0], topFace[1]);
-    glVertex3f(bottomFace[2], bottomFace[0], bottomFace[1]);
-    glVertex3f(topFace[3*14+2], topFace[3*14], topFace[3*14+1]);
+    glNormal3f(sin(offset + 9*dtheta), cos(offset + 9*dtheta), 0);
+    glVertex3f(topFace[3*9], topFace[3*9+1], topFace[3*9+2]);
+    glVertex3f(topFace[0], topFace[1], topFace[2]);
+    glVertex3f(bottomFace[0], bottomFace[1], bottomFace[2]);
+    glVertex3f(topFace[3*9], topFace[3*9+1], topFace[3*9+2]);
   glEnd();
 }
 
@@ -176,6 +190,23 @@ void Hilbert::drawCyl(float len)
 void Hilbert::drawCube(float height)
 {
   glutSolidCube(height);
+}
+
+//Draw a 6-sided green leaf in the xz plane with one tip at the origin
+void Hilbert::drawLeaf(float len)
+{
+  glColor3f(0, 1, 0); //Full green
+  glNormal3f(0, 1, 0);
+  glBegin(GL_POLYGON);
+    glVertex3f(0, 0, 0);
+    glVertex3f(1.732*0.5*len, 0, 0.5*len);
+    glVertex3f((1.732*0.5 + 1)*len, 0, 0.5*len);
+    glVertex3f((1.732 + 1)*len, 0, 0);
+    glVertex3f((1.732*0.5 + 1)*len, 0, -0.5*len);
+    glVertex3f(1.732*0.5*len, 0, -0.5*len);
+  glEnd();
+  glColor3f(0.675, 0.451, 0.224); //Light brown
+  //Ideally we would specify colors for different sections
 }
 
 //Draw a coordinate system with axes of the given length
@@ -227,7 +258,7 @@ void Hilbert::changeColor()
 Note: a character can be both a variable and a constant.  When level is
 not equal to zero, the char will be treated as a variable.  When level
 is equal to zero it will be treated as a constant (provided that it
-represents a valid constant
+represents a valid constant)
 */
 
 void Hilbert::parseStr(string str, int level)
@@ -264,7 +295,7 @@ void Hilbert::parseStr(string str, int level)
         case '|':
           glRotatef(180,0,1,0); break;
         case 'F':
-          changeColor();
+          //changeColor();
           glCallList(shapes[userShape]); 
           break;
         case '`':
@@ -272,6 +303,9 @@ void Hilbert::parseStr(string str, int level)
           break;
         case '~':
           glColor3f(0.5, 0.5, 0.5);
+          break;
+        case '$':
+          glCallList(shapes[LEAF]);
           break;
       }
     }
@@ -285,9 +319,10 @@ void Hilbert::parseStr(string str, int level)
 void Hilbert::init()
 {
   //Basic setup
-  glClearColor(0.5, 0.5, 0.5, 0);
+  glClearColor(0.6, 0.8, 1, 0);
   glShadeModel(GL_FLAT);
   glEnable(GL_DEPTH_TEST);
+  //To draw leaves, culling must be disabled
   glEnable(GL_CULL_FACE);
   glCullFace(GL_BACK);
 
@@ -328,6 +363,11 @@ void Hilbert::init()
     glTranslatef(0, 0, sideLen);
     drawCyl(sideLen);
   glEndList();
+
+  shapes[LEAF] = glGenLists(1);
+  glNewList(shapes[LEAF], GL_COMPILE);
+    drawLeaf(sideLen);
+  glEndList();
 }
 
 void Hilbert::display()
@@ -346,7 +386,8 @@ void Hilbert::display()
     cout << "Rotated by " << deg << " degrees" << endl;
   }
 
-  glColor3f(0.6, 0.8, 1);
+  //Draw a green circle as our base
+  glColor3f(0, 0.8, 0);
   float convert = 3.14159/180;
   glBegin(GL_POLYGON);
     for(int i = 0; i < 360; i += 15)
@@ -354,12 +395,14 @@ void Hilbert::display()
       glVertex3f(-10*cos(i*convert), 0, 10*sin(i*convert));
     }
   glEnd();
-  
+
   glRotatef(deg, 0, 1, 0); //Rotate scene
 
   //Set up color variables
   color = 0.4;
   up = true;
+
+  glColor3f(0.675, 0.451, 0.224); //Light brown
   
   glRotatef(-90, 1, 0, 0); //Set pen to point upwards
   parseStr(startStr, level);
@@ -375,7 +418,7 @@ void Hilbert::reshape(int w, int h)
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(60, (GLfloat)w/h, 1, 60);
+  gluPerspective(60, (GLfloat)w/h, 1, 80);
 
   glMatrixMode(GL_MODELVIEW);
 }
